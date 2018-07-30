5f69c223764554c2e8155cf69c71c598c07dabc9
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 16:30:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  katz_deli == 0? "The line is currently empty." : false
+  katz_deli == 0? puts "The line is currently empty." : false
 end
 
 def take_a_number(katz_deli, name)
