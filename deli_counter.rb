2b3584d1d3974aa65389b72f9e96275e511e2431
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 16:53:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,7 @@ def line(katz_deli)
     other_deli = []
     katz_deli.each.with_index(1) do |name, number|
       other_deli.push ( "#{number}. #{name}")
+      puts " The line is currently {"
     end
     other_deli
   end
